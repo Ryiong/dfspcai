94da0bd39a7b5d52d0bbf275381bf214499a122f
Ryiong <viethung1209.work@gmail.com>
2023-10-27 17:11:03 +0700

    update final code

--- graphicsTesting.py
+++ graphicsTesting.py
@@ -1,20 +1,22 @@
 from graphics import *
 import time
-from threading import Thread
 
-grid_side = 50;
-# create a graphics window
-win = GraphWin ('Testing', 500, 500)
+grid_side = 50
+
+win = GraphWin ('ProjectPacman', 500, 500)
 win.setBackground(color_rgb(255, 255, 255))
+
 wallsList = [(9, 1), (10, 1), (2, 2), (4, 2), (5, 2), (7, 2), (7, 3), (9, 3),
-             (3, 4), (5, 4), (7, 4), (9, 4), (3, 5), (5, 5), (7, 5), (8, 5), (9, 5), (2, 6),
-             (3, 6), (5, 6), (8, 6), (2, 7), (5, 7), (6, 7), (8, 7), (10, 7), (2, 8), (6, 8), (8, 8),
-             (10, 8), (4, 9), (5, 9), (6, 9), (1, 10), (2, 10)]
+             (3, 4), (5, 4), (7, 4), (9, 4), (3, 5), (5, 5), (7, 5), (8, 5),
+             (9, 5), (2, 6), (3, 6), (5, 6), (8, 6), (2, 7), (5, 7), (6, 7),
+             (8, 7), (10, 7), (2, 8), (6, 8), (8, 8), (10, 8), (4, 9), (5, 9),
+             (6, 9), (1, 10), (2, 10)]
 
 adjencyDict = {}
-centerWall = {}
+
 nodeVisit = [[False for i in range(11)] for j in range(11)]
 nodeVisit[1][1] = True
+
 for a in wallsList:
     pt1 = Point((a[0] - 1) * grid_side, (a[1] - 1) * grid_side)
     pt2 = Point((a[0]) * grid_side, (a[1]) * grid_side)
@@ -24,17 +26,22 @@ for a in wallsList:
 
 global endPoint
 endPoint = (1, 4)
-# create a circle object
-circle = Circle(Point(((endPoint[0] - 1) * grid_side) + 25, ((endPoint[1] - 1) * grid_side) + 25), 25)
+
+
+circle = Circle(Point(((endPoint[0] - 1) * grid_side) + 25,
+                      ((endPoint[1] - 1) * grid_side) + 25), 25)
 circle.setFill('yellow')
 circle.draw(win)
+
 global startPoint
 startPoint = (1, 1)
-circle1 = Circle(Point(((startPoint[0] - 1) * grid_side) + 25, ((startPoint[1] - 1) * grid_side) + 25), 25)
+
+circle1 = Circle(Point(((startPoint[0] - 1) * grid_side) + 25,
+                       ((startPoint[1] - 1) * grid_side) + 25), 25)
 circle1.setFill('red')
 circle1.draw(win)
 
-path = [];
+path = []
 
 def createAdjencyDict():
     for y in range(1, 11):
@@ -61,9 +68,9 @@ def createAdjencyDict():
                         currList = adjencyDict[point]
                         currList.insert(0, (y, x + 1))
 
-def moving():
+def moving(key):
     global endPoint
-    key = win.checkKey()
+
     if key == 'Up':
         circle.move (0, -50)
         print("Up")
@@ -73,6 +80,7 @@ def moving():
             rect1 = Rectangle(pt1, pt2)
             if circle.getCenter().x == rect1.getCenter().x and circle.getCenter().y == rect1.getCenter().y:
                 circle.move(0, 50)
+
     elif key == 'Down':
         circle.move (0, 50)
         print("Down")
@@ -81,13 +89,8 @@ def moving():
             pt2 = Point(a[0] * grid_side, a[1] * grid_side)
             rect1 = Rectangle(pt1, pt2)
             if circle.getCenter().x == rect1.getCenter().x and circle.getCenter().y == rect1.getCenter().y:
-                circle.move(0, 50)
-        for a in wallsList:
-            pt1 = Point((a[0] - 1) * grid_side, (a[1] - 1) * grid_side)
-            pt2 = Point(a[0] * grid_side, a[1] * grid_side)
-            rect1 = Rectangle(pt1, pt2)
-            if circle.getCenter().x == rect1.getCenter().x and circle.getCenter().y == rect1.getCenter().y:
                 circle.move(0, -50)
+
     elif key == 'Left':
         circle.move (-50, 0)
         print("Left")
@@ -97,6 +100,7 @@ def moving():
             rect1 = Rectangle(pt1, pt2)
             if circle.getCenter().x == rect1.getCenter().x and circle.getCenter().y == rect1.getCenter().y:
                 circle.move(50, 0)
+
     elif key == 'Right':
         circle.move (50, 0)
         print("Right")
@@ -106,18 +110,22 @@ def moving():
             rect1 = Rectangle(pt1, pt2)
             if circle.getCenter().x == rect1.getCenter().x and circle.getCenter().y == rect1.getCenter().y:
                 circle.move(-50, 0)
+
     endPoint = ((circle.p1.x / grid_side) + 1, (circle.p1.y / grid_side) + 1)
+
     path.clear()
     dfs(startPoint, endPoint)
 
 def dfs(node, target):
     endReached = False
     nodeVisit[node[0]][node[1]] = True
-    path.insert(len(path), node)
+    path.append(node)
+    
     if node == target:
         return True
+    
     for point in adjencyDict[node]:
-        if (endReached == True):
+        if endReached == True:
             break
         if nodeVisit[point[0]][point[1]] == False:
             endReached = dfs(point, target)
@@ -127,25 +135,24 @@ def dfs(node, target):
 
 def moveGhost():
     global startPoint
+
     for i in range(0, len(path) - 1):
         time.sleep(0.1)
-        # Put a circle in next cell:
+
         cell = path[i + 1]
         cellBefore = path[i]
+
         changex = ((cell[0] - startPoint[0]) * grid_side) - ((cellBefore[0] - startPoint[0]) * grid_side)
         changey = ((cell[1] - startPoint[1] - 0.5) * grid_side) - ((cellBefore[1] - startPoint[1] - 0.5) * grid_side)
+        
         circle1.move(changex, changey)
         startPoint = cell
 
 while True:
     nodeVisit = [[False for i in range(11)] for j in range(11)]
     nodeVisit[1][1] = True
+
+    key = win.checkKey()
     createAdjencyDict()
-    moving()
+    moving(key)
     moveGhost()
-    print(startPoint, endPoint, path, nodeVisit)
-
-
-
-
-
--- packmanDFS.py
+++ packmanDFS.py
@@ -1,169 +1,158 @@
 from graphics import *
 import time
 
-grid_side = 50;
-win = GraphWin("ProjectPacman", grid_side*10, grid_side*10);
-#NOTE: In this list and other places, first point is y axis and second is x!!
+grid_side = 50
 
-wallsList = [(1,9), (1,10), (2,2), (2,4), (2,5), (2,7), (3,7), (3,9),
-            (4,3), (4,5), (4,7), (4,9), (5,3), (5,5), (5,7),(5,8),(5,9), (6,2),
-            (6,3),(6,5),(6,8),(7,2),(7,5),(7,6),(7,8),(7,10),(8,2),(8,6),(8,8),
-            (8,10),(9,4),(9,5),(9,6),(10,1),(10,2)];
+win = GraphWin ('ProjectPacman', 500, 500)
+win.setBackground(color_rgb(255, 255, 255))
 
-startPoint = (1,1);
-endPoint = (4,6);
+wallsList = [(9, 1), (10, 1), (2, 2), (4, 2), (5, 2), (7, 2), (7, 3), (9, 3),
+             (3, 4), (5, 4), (7, 4), (9, 4), (3, 5), (5, 5), (7, 5), (8, 5),
+             (9, 5), (2, 6), (3, 6), (5, 6), (8, 6), (2, 7), (5, 7), (6, 7),
+             (8, 7), (10, 7), (2, 8), (6, 8), (8, 8), (10, 8), (4, 9), (5, 9),
+             (6, 9), (1, 10), (2, 10)]
 
-adjencyDict = {};
+adjencyDict = {}
 
-
-#In the beginning all nodes, except start, are not visited.
 nodeVisit = [[False for i in range(11)] for j in range(11)]
-nodeVisit[1][1] = True;
+nodeVisit[1][1] = True
 
-path = [];
-######################################################################
-######################################################################
+for a in wallsList:
+    pt1 = Point((a[0] - 1) * grid_side, (a[1] - 1) * grid_side)
+    pt2 = Point((a[0]) * grid_side, (a[1]) * grid_side)
+    rect1 = Rectangle(pt1, pt2)
+    rect1.setFill(color_rgb(0, 102, 248))
+    rect1.draw(win)
+
+global endPoint
+endPoint = (1, 4)
+
+
+circle = Circle(Point(((endPoint[0] - 1) * grid_side) + 25,
+                      ((endPoint[1] - 1) * grid_side) + 25), 25)
+circle.setFill('yellow')
+circle.draw(win)
+
+global startPoint
+startPoint = (1, 1)
+
+circle1 = Circle(Point(((startPoint[0] - 1) * grid_side) + 25,
+                       ((startPoint[1] - 1) * grid_side) + 25), 25)
+circle1.setFill('red')
+circle1.draw(win)
+
+path = []
 
 def createAdjencyDict():
-   for y in range(1,11):
-      for x in range(1,11):
-         point = (y,x);
-         if point in wallsList:
-            continue;
-         else:
-            adjencyDict[point] = [];
-            if((y-1 != 0)):
-               if (y-1,x) not in wallsList:
-                  currList = adjencyDict[point];
-                  currList.insert(0,(y-1,x));
-            if((y+1 != 11)):
-               if (y+1,x) not in wallsList:
-                  currList = adjencyDict[point];
-                  currList.insert(0,(y+1,x));
-            if((x-1 != 0)):
-               if (y,x-1) not in wallsList:
-                  currList = adjencyDict[point];
-                  currList.insert(0,(y,x-1));
-            if((x+1 != 11)):
-               if (y,x+1) not in wallsList:
-                  currList = adjencyDict[point];
-                  currList.insert(0,(y,x+1));
-               
-######################################################################
-######################################################################
-                  
-def initializeGame():
-   """
-   This function initializes the board with walls and place the pacman at start and
-   marks the end position.
+    for y in range(1, 11):
+        for x in range(1, 11):
+            point = (y, x)
+            if point in wallsList:
+                continue
+            else:
+                adjencyDict[point] = []
+                if ((y - 1 != 0)):
+                    if (y - 1, x) not in wallsList:
+                        currList = adjencyDict[point]
+                        currList.insert(0, (y - 1, x))
+                if ((y + 1 != 11)):
+                    if (y + 1, x) not in wallsList:
+                        currList = adjencyDict[point]
+                        currList.insert(0, (y + 1, x))
+                if ((x - 1 != 0)):
+                    if (y, x - 1) not in wallsList:
+                        currList = adjencyDict[point]
+                        currList.insert(0, (y, x - 1))
+                if ((x + 1 != 11)):
+                    if (y, x + 1) not in wallsList:
+                        currList = adjencyDict[point]
+                        currList.insert(0, (y, x + 1))
 
-   """
-   #Coloring the background black
-   win.setBackground(color_rgb(0,0,0)); 
+def moving(key):
+    global endPoint
 
-   for i in range(1,11):
-      for j in range(1,11):
-         center = Point((i-0.5)*grid_side,(j-0.5)*grid_side);
-         cir = Circle(center, 1);
-         cir.setFill(color_rgb(255,255,255))
-         cir.draw(win)
+    if key == 'Up':
+        circle.move (0, -50)
+        print("Up")
+        for a in wallsList:
+            pt1 = Point((a[0] - 1) * grid_side, (a[1] - 1) * grid_side)
+            pt2 = Point(a[0] * grid_side, a[1] * grid_side)
+            rect1 = Rectangle(pt1, pt2)
+            if circle.getCenter().x == rect1.getCenter().x and circle.getCenter().y == rect1.getCenter().y:
+                circle.move(0, 50)
 
-   #Drawing the walls;
-   for a in wallsList:
-      pt1 = Point((a[1]-1)*grid_side, (a[0]-1)*grid_side)
-      pt2 = Point((a[1])*grid_side, (a[0])*grid_side)
-      rect1 = Rectangle(pt1, pt2)
-      rect1.setFill(color_rgb(0,102,248))
-      rect1.draw(win)
+    elif key == 'Down':
+        circle.move (0, 50)
+        print("Down")
+        for a in wallsList:
+            pt1 = Point((a[0] - 1) * grid_side, (a[1] - 1) * grid_side)
+            pt2 = Point(a[0] * grid_side, a[1] * grid_side)
+            rect1 = Rectangle(pt1, pt2)
+            if circle.getCenter().x == rect1.getCenter().x and circle.getCenter().y == rect1.getCenter().y:
+                circle.move(0, -50)
 
+    elif key == 'Left':
+        circle.move (-50, 0)
+        print("Left")
+        for a in wallsList:
+            pt1 = Point((a[0] - 1) * grid_side, (a[1] - 1) * grid_side)
+            pt2 = Point(a[0] * grid_side, a[1] * grid_side)
+            rect1 = Rectangle(pt1, pt2)
+            if circle.getCenter().x == rect1.getCenter().x and circle.getCenter().y == rect1.getCenter().y:
+                circle.move(50, 0)
 
+    elif key == 'Right':
+        circle.move (50, 0)
+        print("Right")
+        for a in wallsList:
+            pt1 = Point((a[0] - 1) * grid_side, (a[1] - 1) * grid_side)
+            pt2 = Point(a[0] * grid_side, a[1] * grid_side)
+            rect1 = Rectangle(pt1, pt2)
+            if circle.getCenter().x == rect1.getCenter().x and circle.getCenter().y == rect1.getCenter().y:
+                circle.move(-50, 0)
 
-   center = Point((startPoint[1]-0.5)*grid_side,(startPoint[0]-0.5)*grid_side);
-   cir = Circle(center, 25);
-   cir.setFill(color_rgb(255,255,0))
-   cir.draw(win)
-   
-   
-   rect1 = Rectangle(Point((endPoint[1]-1)*grid_side, (endPoint[0]-1)*grid_side),
-                     Point((endPoint[1])*grid_side, (endPoint[0])*grid_side))
-   rect1.setFill(color_rgb(255,0,0))
-   rect1.draw(win)
+    endPoint = ((circle.p1.x / grid_side) + 1, (circle.p1.y / grid_side) + 1)
 
-######################################################################
-######################################################################
-# DFS algorithm:
-#node is coordinate in (y,x) way.
-def explore(node):
-   
-   endReached = False;
-   nodeVisit[node[0]][node[1]] = True;
-   path.insert(len(path), node);
-   
-   if node ==  endPoint:
-      return True;
+    path.clear()
+    dfs(startPoint, endPoint)
 
-   if node != startPoint:
-      colorNode(node,0,175,0); # Green
-   
-   for neighbour in adjencyDict[node]:
-      if(endReached == True):
-         break;
-      if (nodeVisit[neighbour[0]][neighbour[1]] == False):
-         endReached = explore(neighbour);
-         if(not endReached):
-            path.insert(len(path), node);
-            if node != startPoint:
-               colorNode(node,0,175,0);
-         
-   return endReached;
-
-def colorNode(node,r,g,b):
-   pt1 = Point((node[1]-1)*grid_side, (node[0]-1)*grid_side)
-   pt2 = Point((node[1])*grid_side, (node[0])*grid_side)
-   rect1 = Rectangle(pt1, pt2)
-   rect1.setFill(color_rgb(r,g,b))
-   rect1.draw(win)
-
-def colorPathBlack():
-   for node in path:
-      pt1 = Point((node[1]-1)*grid_side, (node[0]-1)*grid_side)
-      pt2 = Point((node[1])*grid_side, (node[0])*grid_side)
-      rect1 = Rectangle(pt1, pt2)
-      rect1.setFill(color_rgb(0,0,0))
-      rect1.draw(win)
-      
-      rect1 = Rectangle(Point((endPoint[1]-1)*grid_side, (endPoint[0]-1)*grid_side),
-                     Point((endPoint[1])*grid_side, (endPoint[0])*grid_side))
-      rect1.setFill(color_rgb(255,0,0))
-      rect1.draw(win)
+def dfs(node, target):
+    endReached = False
+    nodeVisit[node[0]][node[1]] = True
+    path.append(node)
+    
+    if node == target:
+        return True
+    
+    for point in adjencyDict[node]:
+        if endReached == True:
+            break
+        if nodeVisit[point[0]][point[1]] == False:
+            endReached = dfs(point, target)
+            if not endReached:
+                path.insert(len(path), node)
+    return endReached
 
 def moveGhost():
-   for i in range(0, len(path) - 1):
-      time.sleep(0.1);
-      #Put a circle in next cell:
-      cell = path[i+1];
-      center = Point((cell[1]-0.5)*grid_side,(cell[0]-0.5)*grid_side);
-      cir = Circle(center, 25);
-      cir.setFill(color_rgb(255,255,0))
-      cir.draw(win)
+    global startPoint
 
-      #Turn current circle green
-      colorNode(path[i],255,20,147);
-      
+    for i in range(0, len(path) - 1):
+        time.sleep(0.1)
 
-def main():
-   initializeGame();
-   createAdjencyDict();
-   explore(startPoint);
-   #scolorPathBlack()
-   moveGhost();
-   
-   #getMounse() + close(): It wait untilsomeone clicks and doing so closes it
-   win.getMouse();
-   win.close();
+        cell = path[i + 1]
+        cellBefore = path[i]
 
+        changex = ((cell[0] - startPoint[0]) * grid_side) - ((cellBefore[0] - startPoint[0]) * grid_side)
+        changey = ((cell[1] - startPoint[1] - 0.5) * grid_side) - ((cellBefore[1] - startPoint[1] - 0.5) * grid_side)
+        
+        circle1.move(changex, changey)
+        startPoint = cell
 
-   
-main();
+while True:
+    nodeVisit = [[False for i in range(11)] for j in range(11)]
+    nodeVisit[1][1] = True
 
-
+    key = win.checkKey()
+    createAdjencyDict()
+    moving(key)
+    moveGhost()
